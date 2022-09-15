cfff682969308fbd4805c34234cb74871d4b9f6f
Sarfraz <sarfrazmohsin17@gmail.com>
2022-09-15 22:38:36 +0530

    Initial commit

---
+++ javascriptBasics.js
@@ -0,0 +1,16 @@
+//in-line comments
+/* multiline commnets*/
+/*Data types in js- undifined, null, boolean, string, symbol, number and object*/
+console.log("hello world")
+
+//Declaration of a data type can be done by 3 ways- var(it can be used throughout the whole program), let(it can only be used inside a scope), and const(it can never be changed)
+
+var myName= "Sarfraz"
+let ourName= "Mohsin"
+const pi= 3.14
+
+//adding, subtracting, multiplying, dividing two numbers in js is
+var sum= 10+2;
+var sub= 10-4;
+var mul= 10*4;
+var div=10/5;
